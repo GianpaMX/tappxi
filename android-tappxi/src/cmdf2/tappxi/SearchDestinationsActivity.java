df6f8310e9273cb1172039dadf705e5096dcab29
Juan Villa <juan64@gmail.com>
2012-09-06 00:02:47 -0500

    search widget

--- android-tappxi/src/cmdf2/tappxi/SearchDestinationsActivity.java
+++ android-tappxi/src/cmdf2/tappxi/SearchDestinationsActivity.java
@@ -1,6 +1,13 @@
 package cmdf2.tappxi;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.SearchView;
 
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
@@ -14,6 +21,27 @@ public class SearchDestinationsActivity extends MapActivity {
         
         MapView mapView = (MapView) findViewById(R.id.mapview);
         mapView.setBuiltInZoomControls(true);
+        
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+          String query = intent.getStringExtra(SearchManager.QUERY);
+          Log.d("tappxi", query);
+        }
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the options menu from XML
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_search_destinations, menu);
+
+        // Get the SearchView and set the searchable configuration
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.menu_search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
+
+        return true;
     }
 
 	@Override
